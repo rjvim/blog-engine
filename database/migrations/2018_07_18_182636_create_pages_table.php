a4c4a2159e43620c81d51b2bc3cd2867de5f33c9
chan201 <chandrakanth@betalectic.com>
2018-07-19 00:12:28 +0530

    added pages table

---
+++ database/migrations/2018_07_18_182636_create_pages_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pages', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+            $table->string('name');
+            $table->string('slug');
+            $table->integer('publishid_at');
+            $table->string('status');
+            $table->integer('autor_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pages');
+    }
+}
